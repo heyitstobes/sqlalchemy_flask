d9f029bbe6dc17e6d46d782fc793d48cd8020020
heyitstobes <57779270+heyitstobes@users.noreply.github.com>
2020-01-10 22:45:41 -0500

    Add files via upload

---
+++ sqlalchemy_hw/app.py
@@ -0,0 +1,75 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/"
+        f"/api/v1.0/tobs<br/"
+        f"/api/v1.0/<start><br/"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precip():
+    session = Session(engine)
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= '2016-08-23').all()
+    session.close()
+    dict= {date: prcp for date, prcp in results}
+    return jsonify(dict)
+
+@app.route("/api/v1.0/stations")
+def stations():
+  
+    session = Session(engine)
+    stations = session.query(Station.station).all()
+    session.close()
+
+    return jsonify(stations)
+
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+
+    results = session.query(Measurement.tobs).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= '2016-08-23').all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
+
+
+if __name__ == '__main__':
+    app.run()
